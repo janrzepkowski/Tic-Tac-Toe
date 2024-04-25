cfbd1f640e3922f974f843e4e71cc56ca56e05a3
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-04-25 11:40:17 +0200

    Handle Tile Click

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import {useState} from "react";
 import Board from "./Board";
 
 function TicTacToe() {
+
+    const PLAYER_X = "X";
+    const PLAYER_O = "O";
+
+    const [tiles, setTiles] = useState(Array(9).fill(null));
+    const [player, setPlayer] = useState(PLAYER_X);
+
+    const handleTileClick = (index) => {
+        if (tiles[index] !== null) {
+            return;
+        }
+
+        const newTiles = [...tiles];
+        newTiles[index] = player;
+        setTiles(newTiles);
+        if (player === PLAYER_X) {
+            setPlayer(PLAYER_O);
+        } else {
+            setPlayer(PLAYER_X);
+        }
+    }
+
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            <Board />
+            <Board tiles={tiles} onTileClick={handleTileClick}/>
         </div>
     );
 }
